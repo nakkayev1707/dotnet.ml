c99ea6524bc3dbf69b1538c91dad07b5f1d4d2c6
Ismail <nakkazulav@mail.ru>
2025-02-15 01:28:13 +0400

    init

---
+++ dotnet.ml/Program.cs
@@ -0,0 +1,16 @@
+﻿using dotnet.ml.QA;
+using Microsoft.ML;
+
+static class Program
+{
+    public static void Main()
+    {
+        List<string> tutorialTexts = TutorialQA.LoadTutorials();
+        ITransformer model = TutorialQA.TrainModel(tutorialTexts);
+        
+        Console.WriteLine("Ask a question:");
+        string question = Console.ReadLine();
+        string answer = TutorialQA.FindBestMatch(question, tutorialTexts, model);
+        Console.WriteLine($"Best Answer: {answer}");
+    }
+}
---
+++ dotnet.ml/Setup/MLSetup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Calibrators;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+
+namespace dotnet.ml.Setup;
+
+public class MLSetup
+{
+    private MLContext _context = new();
+    
+    // private ITransformer Train()
+    // {
+    //     IDataView trainingData = _context.Data
+    //         .LoadFromTextFile<Input>("path_to_file", hasHeader: true);
+    //     
+    //     var pipeline = _context.Transforms.Text
+    //         .FeaturizeText("Features", nameof(SentimentIssue.Text))
+    //         .Append(_context.BinaryClassification.Trainers
+    //             .LbfgsLogisticRegression("Label", "Features"));
+    //     
+    //     ITransformer trainedModel = pipeline.Fit(trainingData);
+    //
+    //     return trainedModel;
+    // }
+
+    public void Evaluate()
+    {
+        
+    }
+
+    public void Embedding()
+    {
+        var path = "D:\\MY-FILES\\Development\\.NET\\dotnet.ml\\dotnet.ml\\Setup\\data.csv";
+        IDataView? dataView = _context.Data.LoadFromTextFile<Input>(path, hasHeader: true);
+
+        // Define the pipeline
+        var pipeline = _context.Transforms.Text
+            .FeaturizeText("Features", "Text")
+            .Append(_context.BinaryClassification.Trainers
+                .SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+
+        // Train the model
+        var model = pipeline.Fit(dataView);
+
+        // Make predictions
+        var predictor = _context.Model.CreatePredictionEngine<Input, Prediction>(model);
+        var prediction = predictor.Predict(new Input() { Text = "I absolutely like this product" });
+        Console.WriteLine($"Predicted Label: {prediction.PredictedLabel}");
+    }
+
+    public void Predict(ITransformer trainedModel)
+    {
+        var predictionEngine = _context.Model
+            .CreatePredictionEngine<Input, Output>(trainedModel);
+
+        var sampleStatement = new Input() { Text = "This is a horrible movie" };
+
+        var prediction = predictionEngine.Predict(sampleStatement);
+        Console.WriteLine(prediction);
+    }
+}
